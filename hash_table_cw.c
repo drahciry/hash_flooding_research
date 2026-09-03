a87e2d933279ba8656a621ddaff1b2d8c0aa3ebe
drahciry <richardgonric@gmail.com>
2026-09-03 11:11:45 -0300

    fix & update: memory leaks removed and comparison between strings are not limited by length more; raw_hash added to Node struct to optimize research and rehash

--- hash_table_cw.c
+++ hash_table_cw.c
@@ -1,3 +1,7 @@
+/**
+ * Important Includes
+ */
+
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,10 +9,20 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define PRIME 4294967291ULL
+/**
+ * Macro definitions
+ */
 
-#define EMPTY 0
-#define OCCUPIED 1
+#define PRIME 4294967291ULL
+#define DEFAULT_KEY_LENGTH 64
+#define LOAD_FACTOR_THRESHOLD 0.75
+
+/**
+ * Structs definitions:
+ *     - Carter Wegman Hasher
+ *     - Node to Linked List
+ *     - Hash Table
+ */
 
 typedef struct {
     uint64_t* coefficients;
@@ -16,18 +30,86 @@ typedef struct {
 } CarterWegmanHasher;
 
 typedef struct Node {
-    uint8_t* key;
-    uint64_t item;
+    char* key;
+    int64_t item;
+    uint32_t hash;
     struct Node* next;
 } Node;
 
 typedef struct {
     Node** buckets;
-    uint8_t* status;
     size_t size;
     size_t capacity;
     CarterWegmanHasher* hasher;
 } HashTable;
+
+/**
+ * Auxiliary functions:
+ *     - isPrime: returns if a number is prime
+ *     - nextPrime: returns next prime based on entry
+ *     - freeNode: free memory with Node struct
+ *     - cw_destroy: free memory with Carter Wegman Hasher struct
+ *     - buckest_destroy: free all memory with Node struct
+ *     - ht_destroy: free memory with Hash Table struct
+ *     - cw_create: allocate memory to Carter Wegman Hasher struct
+ *     - ht_create: allocate memory to Hash Table struct
+ *     - ensure_capacity: ensure that Carter Wegman Hasher capacity is sufficient
+ */
+
+bool isPrime(int64_t num) {
+    if (num <= 1) return false;
+
+    for (size_t i = 2; i * i <= num; i++)
+        if (num % i == 0)
+            return false;
+
+    return true;
+}
+
+int64_t nextPrime(int64_t num) {
+    if (num <= 0) num = 100;
+
+    int64_t prime = num;
+
+    while (!nextPrime(prime))
+        prime++;
+
+    return prime;
+}
+
+void freeNode(Node* node) {
+    free(node->key);
+    free(node);
+}
+
+void cw_destroy(CarterWegmanHasher* hasher) {
+    if (hasher) {
+        free(hasher->coefficients);
+        free(hasher);
+    }
+}
+
+void buckets_destroy(HashTable* hash_table) {
+    for (size_t i = 0; i < hash_table->capacity; i++) {
+        if (hash_table->buckets[i] != NULL) {
+            Node* current = hash_table->buckets[i];
+            while (current) {
+                Node* next = current->next;
+                freeNode(current);
+                current = next;
+            }
+        }
+    }
+}
+
+void ht_destroy(HashTable* hash_table) {
+    if (hash_table) {
+        cw_destroy(hash_table->hasher);
+        buckets_destroy(hash_table);
+        free(hash_table->buckets);
+        free(hash_table);
+    }
+}
 
 CarterWegmanHasher* cw_create(size_t initial_capacity) {
     CarterWegmanHasher* hasher = (CarterWegmanHasher*)malloc(sizeof(CarterWegmanHasher));
@@ -54,22 +136,16 @@ HashTable* ht_create(size_t initial_capacity) {
     HashTable* hash_table = (HashTable*)malloc(sizeof(HashTable));
     if (!hash_table) return NULL;
 
+    initial_capacity = nextPrime(initial_capacity);
+
     hash_table->size = 0;
     hash_table->capacity = initial_capacity;
 
     hash_table->buckets = (Node**)calloc(hash_table->capacity, sizeof(Node*));
-    hash_table->status = (uint8_t*)calloc(hash_table->capacity, sizeof(uint8_t));
-    if (!hash_table->buckets || !hash_table->status) {
+    hash_table->hasher = cw_create(DEFAULT_KEY_LENGTH);
+    if (!hash_table->buckets || !hash_table->hasher) {
         if (hash_table->buckets) free(hash_table->buckets);
-        if (hash_table->status) free(hash_table->status);
-        free(hash_table);
-        return NULL;
-    }
-
-    hash_table->hasher = cw_create(hash_table->capacity);
-    if (!hash_table->hasher) {
-        free(hash_table->buckets);
-        free(hash_table->status);
+        if (hash_table->hasher) cw_destroy(hash_table->hasher);
         free(hash_table);
         return NULL;
     }
@@ -100,35 +176,67 @@ bool ensure_capacity(CarterWegmanHasher* hasher, uint64_t required_capacity) {
     return true;
 }
 
-uint32_t cw_hash(CarterWegmanHasher* hasher, const uint8_t* data, size_t len) {
-    if (len == 0) return 0;
+/**
+ * Main functions:
+ *     - cw_hash: generates a hash to string key with Carter Wegman Hasher
+ *     - rehash: resizes the capacity to Hash Table struct
+ *     - insertItem: inserts a item with key in Hash Table
+ *     - deleteItem: deletes a item based on string key
+ *     - getItem: returns item based on string key
+ */
 
+bool cw_hash(CarterWegmanHasher* hasher, const char* data, size_t len, uint32_t* raw_hash) {
+    if (len == 0) return false;
+    
     if (!ensure_capacity(hasher, len)) {
         fprintf(stderr, "Allocation memory error to expand coefficients.\n");
-        return 0;
+        return false;
     }
 
     uint64_t accum = 0;
-    for (size_t i = 0; i < len; i++) {
-        uint64_t term = (hasher->coefficients[i] * data[i]) % PRIME;
-        accum = (accum + term) % PRIME;
-    }
-
-    return (uint32_t)accum;
+    for (size_t i = 0; i < len; i++)
+        accum += hasher->coefficients[i] * (uint8_t)data[i];
+    
+    *raw_hash = (uint32_t)(accum % PRIME);
+    return true;
 }
 
-bool insertItem(HashTable* hash_table, const uint8_t* key, uint64_t item) {
-    double loadFactor = (double)hash_table->size / hash_table->capacity;
-    if (loadFactor >= 0.75) {};
+bool rehash(HashTable* hash_table) {
+    uint64_t new_capacity = nextPrime(hash_table->capacity << 1);
+    Node** new_buckets = (Node**)calloc(new_capacity, sizeof(Node*));
+    if (!new_buckets) return false;
 
-    size_t len = strlen(key);
-    uint32_t raw_hash = cw_hash(hash_table->hasher, key, len);
+    for (size_t i = 0; i < hash_table->capacity; i++) {
+        Node* current = hash_table->buckets[i];
+        while (current) {
+            Node* next = current->next;
+
+            size_t index = current->hash % new_capacity;
+            current->next = new_buckets[index];
+            new_buckets[index] = current;
+
+            current = next;
+        }
+    }
+
+    free(hash_table->buckets);
+    hash_table->buckets = new_buckets;
+    hash_table->capacity = new_capacity;
+    return true;
+}
+
+bool insertItem(HashTable* hash_table, const char* key, int64_t item) {
+    double loadFactor = (double)hash_table->size / hash_table->capacity;
+    if (loadFactor >= LOAD_FACTOR_THRESHOLD) rehash(hash_table);
+
+    uint32_t raw_hash;
+    if (!cw_hash(hash_table->hasher, key, strlen(key), &raw_hash))
+        return false;
     size_t index = raw_hash % hash_table->capacity;
-    if (!index) return false;
 
     Node** current = &(hash_table->buckets[index]);
     while (*current) {
-        if (!strncmp((*current)->key, key, len)) {
+        if ((*current)->hash == raw_hash && !strcmp((*current)->key, key)) {
             (*current)->item = item;
             return true;
         }
@@ -141,37 +249,30 @@ bool insertItem(HashTable* hash_table, const uint8_t* key, uint64_t item) {
     Node* newNode = *current;
     newNode->key = strdup(key);
     newNode->item = item;
+    newNode->hash = raw_hash;
     newNode->next = NULL;
 
     hash_table->size++;
-    hash_table->status[index] = OCCUPIED;
 
     return true;
 }
 
-void freeNode(Node* node) {
-    free(node->key);
-    free(node);
-}
-
-bool deleteItem(HashTable* hash_table, const uint8_t* key) {
-    size_t len = strlen(key);
-    uint32_t raw_hash = cw_hash(hash_table->hasher, key, len);
+bool deleteItem(HashTable* hash_table, const char* key) {
+    uint32_t raw_hash;
+    if (!cw_hash(hash_table->hasher, key, strlen(key), &raw_hash))
+        return false;
     size_t index = raw_hash % hash_table->capacity;
-    if (!index) return false;
 
-    if (hash_table->status[index] == EMPTY)
+    if (hash_table->buckets[index] == NULL)
         return false;
 
     Node** current = &(hash_table->buckets[index]);
     while (*current) {
-        if (!strncmp((*current)->key, key, len)) {
+        if ((*current)->hash == raw_hash && !strcmp((*current)->key, key)) {
             Node* temp = *current;
             *current = temp->next;
             freeNode(temp);
             hash_table->size--;
-            if (!hash_table->buckets[index])
-                hash_table->status[index] = EMPTY;
             return true;
         }
         current = &((*current)->next);
@@ -180,18 +281,18 @@ bool deleteItem(HashTable* hash_table, const uint8_t* key) {
     return false;
 }
 
-bool getItem(HashTable* hash_table, const uint8_t* key, uint64_t* out_item) {
-    size_t len = strlen(key);
-    uint32_t raw_hash = cw_hash(hash_table->hasher, key, len);
+bool getItem(HashTable* hash_table, const char* key, int64_t* out_item) {
+    uint32_t raw_hash;
+    if (!cw_hash(hash_table->hasher, key, strlen(key), &raw_hash))
+        return false;
     size_t index = raw_hash % hash_table->capacity;
-    if (!index) return false;
 
-    if (hash_table->status[index] == EMPTY)
+    if (hash_table->buckets[index] == NULL)
         return false;
 
     Node** current = &(hash_table->buckets[index]);
     while (*current) {
-        if (!strncmp((*current)->key, key, len)) {
+        if ((*current)->hash == raw_hash && !strcmp((*current)->key, key)) {
             *out_item = (*current)->item;
             return true;
         }
@@ -199,62 +300,4 @@ bool getItem(HashTable* hash_table, const uint8_t* key, uint64_t* out_item) {
     }
 
     return false;
-}
-
-void cw_destroy(CarterWegmanHasher* hasher) {
-    if (hasher) {
-        free(hasher->coefficients);
-        free(hasher);
-    }
-}
-
-void buckets_destroy(HashTable* hash_table) {
-    for (size_t i = 0; i < hash_table->capacity; i++) {
-        if (hash_table->status[i] == OCCUPIED) {
-            Node* current = hash_table->buckets[i];
-            while (current) {
-                Node* next = current->next;
-                freeNode(current);
-                current = next;
-            }
-        }
-    }
-}
-
-void ht_destroy(HashTable* hash_table) {
-    if (hash_table) {
-        cw_destroy(hash_table->hasher);
-        buckets_destroy(hash_table);
-        free(hash_table->buckets);
-        free(hash_table->status);
-        free(hash_table);
-    }
-}
-
-int main(int argc, char* argv[]) {
-    HashTable* hash_table = ht_create(47);
-
-    insertItem(hash_table, "chave1", 1);
-    insertItem(hash_table, "chave2", 2);
-    insertItem(hash_table, "chave3", 3);
-    insertItem(hash_table, "chave4", 4);
-
-    uint64_t item;
-    if (getItem(hash_table, "chave2", &item))
-        printf("Chave encontrada: %d\n", item);
-    else
-        printf("Chave nao encontrada\n");
-
-    deleteItem(hash_table, "chave2");
-    deleteItem(hash_table, "chave4");
-
-    if (getItem(hash_table, "chave2", &item))
-        printf("Chave encontrada: %d\n", item);
-    else
-        printf("Chave nao encontrada\n");
-
-    ht_destroy(hash_table);
-    hash_table = NULL;
-
-    return 0;
 }
